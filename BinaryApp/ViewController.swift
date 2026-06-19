482dc4c702be17e154c30f2bc53dc592232451fb
Gastón Máspero <gaston.maspero.bambino@gmail.com>
2026-06-19 21:37:24 +0200

    Initial Commit

---
+++ BinaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BinaryApp
+//
+//  Created by Gaston Angel Maspero on 19/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BinaryAppTests/BinaryAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  BinaryAppTests.swift
+//  BinaryAppTests
+//
+//  Created by Gaston Angel Maspero on 19/06/2026.
+//
+
+import Testing
+@testable import BinaryApp
+
+struct BinaryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
